579173877ebc286c4fbb5cf0710dea5ad035a926
teru <74332407+Iketerumanato@users.noreply.github.com>
2024-08-31 20:00:47 +0900

    値の初期化

--- UseUniRx/Assets/Script/PlayerUIController.cs
+++ UseUniRx/Assets/Script/PlayerUIController.cs
@@ -6,8 +6,14 @@ public class PlayerUIController : MonoBehaviour
     public ReactiveProperty<int> PlayerHp = new(100);
     [SerializeField] int DamagePoint = 10;
 
+    private void OnEnable()
+    {
+        PlayerHp.Value = 100;
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) PlayerHp.Value -= DamagePoint;
+        if (PlayerHp.Value <= 0) OnEnable();
     }
 }
